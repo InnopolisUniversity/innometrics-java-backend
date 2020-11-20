5ebd18c92975b56c7eed5613b77ba81e377d6198
ar.kozlov <ar.kozlov@innopolis.ru>
2020-11-20 17:01:59 +0300

    add repositories for models

---
+++ agents-gateway/src/main/java/com/innopolis/innometrics/agentsgateway/repository/sonarqube/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.innopolis.innometrics.agentsgateway.repository.sonarqube;
+
+import com.innopolis.innometrics.agentsgateway.entity.sonarqube.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    List<Project> findByIdIn(List<Long> projectIds);
+
+}
