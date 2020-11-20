dd78a66edb0f6ff652fdd048c1aeb06f55ff6515
ar.kozlov <ar.kozlov@innopolis.ru>
2020-11-20 17:00:13 +0300

    add models for sonarqube tables

---
+++ agents-gateway/src/main/java/com/innopolis/innometrics/agentsgateway/entity/sonarqube/Coverage.java
@@ -0,0 +1,26 @@
+package com.innopolis.innometrics.agentsgateway.entity.sonarqube;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coverage", schema = "sonaragent")
+@Data
+public class Coverage implements Metric {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "branch_coverage")
+    private String branchCoverage;
+
+    @Column(name = "coverage")
+    private String coverage;
+
+    @Column(name = "line_coverage")
+    private String lineCoverage;
+
+    @Column(name = "analysisid")
+    private Long analysisid;
+}
---
+++ agents-gateway/src/main/java/com/innopolis/innometrics/agentsgateway/entity/sonarqube/Sizeandcomplexity.java
@@ -0,0 +1,29 @@
+package com.innopolis.innometrics.agentsgateway.entity.sonarqube;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sizeandcomplexity", schema = "sonaragent")
+@Data
+public class Sizeandcomplexity implements Metric {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "classes")
+    private String classes;
+
+    @Column(name = "complexity_in_classes")
+    private String complexityInClasses;
+
+    @Column(name = "lines")
+    private String lines;
+
+    @Column(name = "ncloc")
+    private String ncloc;
+
+    @Column(name = "analysisid")
+    private Long analysisid;
+}
